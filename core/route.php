9fbf670babc0404341167d803490ce939bfe698e
dhso <dhso@163.com>
2021-10-31 17:27:43 +0800

    Update TBK

--- core/route.php
+++ core/route.php
@@ -461,7 +461,7 @@ function _getTbkFavorites($self)
 
         Typecho_Response::getInstance()->throwJson([
             "code" => 1,
-            "data" => $resp["result_list"]
+            "data" => $resp
         ]);
     } catch (Exception $e) {
         Typecho_Response::getInstance()->throwJson([
